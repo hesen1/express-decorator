bff5edf26035f82bd326af119636593fe0237740
sen.he <hesen@gagogroup.com>
2019-12-24 20:57:37 +0800

    fix: 修复Path中间件错误

--- lib/register-routes.ts
+++ lib/register-routes.ts
@@ -19,7 +19,10 @@ export interface Router extends core.Router {
  * @param rootUrl 根路由
  * @param midwares rootUrl路由中间件
  */
-export function registerRoutes(router: Router, controllers: any[], rootUrl?: string, ...midwares: (core.RequestHandler[] | core.RequestHandler)[]): void {
+export function registerRoutes(
+  router: Router, controllers: any[], rootUrl?: string,
+  ...midwares: (core.RequestHandler[] | core.RequestHandler)[]
+): void {
   rootUrl = rootUrl || "";
   if (midwares && midwares.length) { //  && rootUrl
     midwares = expansion(...midwares);
@@ -33,45 +36,13 @@ export function registerRoutes(router: Router, controllers: any[], rootUrl?: str
     const routes: any = target.prototype._routes;
     for (let key of keys) {
       key = String(key);
-      if (key === "baseUrl") {
+      if (key === "baseUrl" || key === "baseMidwares") {
         continue;
       }
+
       for (const item of routes[key]) {
         item.midwares.push(instance[key]);
-        router[item.method](`${rootUrl}${routes.baseUrl}${item.subUrl}`, ...item.midwares);
-      }
-    }
-  }
-}
-
-export function autoRegisterRoutes(router: Router, dirPath: string, rootPath?: string, ...midwares: (core.RequestHandler[] | core.RequestHandler)[]): void {
-  if (typeof dirPath !== "string" || !dirPath.length) throw new Error("need controller directory path");
-  dirPath = path.normalize(dirPath);
-  rootPath = rootPath || "";
-
-  if (!fs.existsSync(dirPath)) throw new Error("directory does not exist");
-  const filenames: string[] = fs.readdirSync(dirPath);
-
-  if (midwares && midwares.length) {
-    midwares = expansion(...midwares);
-    for (const midware of midwares) {
-      rootPath ? router.use(rootPath, midware) : router.use(midware);
-    }
-  }
-
-  for (const file of filenames) {
-    const target = vm.runInNewContext(`(() => { return require('${file.substring(0, file.lastIndexOf(".") + 1)}') });`);
-    const instance = new target();
-    const keys: any[] = Reflect.ownKeys(target.prototype._routes);
-    const routes: any = target.prototype._routes;
-    for (let key of keys) {
-      key = String(key);
-      if (key === "baseUrl") {
-        continue;
-      }
-      for (const item of routes[key]) {
-        item.midwares.push(instance[key]);
-        router[item.method](`${rootPath}${routes.baseUrl}${item.subUrl}`, ...item.midwares);
+        router[item.method](`${rootUrl}${routes.baseUrl}${item.subUrl}`, ...routes.baseMidwares, ...item.midwares);
       }
     }
   }
